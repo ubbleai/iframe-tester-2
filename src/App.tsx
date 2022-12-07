10b6f52a303cd4fb30e924a889b4bb1c9a2812af
jean-baptiste <jean-baptiste@ubble.ai>
2022-12-07 14:04:29 +0100

    fix: fix script import

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,33 @@
-import { useEffect, useState } from 'react'
-import { useSelector } from 'react-redux'
+import { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
 
-import { AppShell, Divider, Loader, Navbar } from '@mantine/core'
+import { AppShell, Divider, Loader, Navbar } from '@mantine/core';
 
-import { Header } from './features/commons/components/header'
-import { Iframe } from './features/iframe/components/Iframe'
-import { IframeCode } from './features/iframe/components/iframe-code'
-import { IframeError } from './features/iframe/components/iframe-error'
-import { IframeSettings } from './features/iframe/components/iframe-settings'
-import { selectIframeProps } from './features/iframe/store/slice'
-import { LogsDrawer } from './features/logs/components/logs-drawer'
-
-const SDK_URL =
-  'https://oos.eu-west-2.outscale.com/public-ubble-ai/iframe-sdk-0.0.3.js'
+import { Header } from './features/commons/components/header';
+import { Iframe } from './features/iframe/components/Iframe';
+import { IframeCode } from './features/iframe/components/iframe-code';
+import { IframeError } from './features/iframe/components/iframe-error';
+import { IframeSettings } from './features/iframe/components/iframe-settings';
+import { selectIframeProps } from './features/iframe/store/slice';
+import { LogsDrawer } from './features/logs/components/logs-drawer';
 
 declare global {
   interface Window {
-    onUbbleReady: () => void
+    ubbleLoaded: Promise<void>;
   }
 }
 
-function App () {
-  const [navbarOpen, setNavbarOpen] = useState(false)
-  const [scriptLoaded, setScriptLoaded] = useState(false)
+function App() {
+  const [navbarOpen, setNavbarOpen] = useState(false);
+  const [scriptLoaded, setScriptLoaded] = useState(false);
 
-  const iframeProps = useSelector(selectIframeProps)
+  const iframeProps = useSelector(selectIframeProps);
   useEffect(() => {
     (async () => {
-      const awaitUbbleLoaded = new Promise<void>((resolve, reject) => {
-        window.onUbbleReady = () => {
-          resolve()
-        }
-      })
-
-      await Promise.all([await import(SDK_URL), awaitUbbleLoaded])
-      setScriptLoaded(true)
-    })()
-  }, [])
+      await window.ubbleLoaded;
+      setScriptLoaded(true);
+    })();
+  }, []);
 
   return (
     <AppShell
@@ -67,7 +58,7 @@ function App () {
 
       <LogsDrawer />
     </AppShell>
-  )
+  );
 }
 
-export default App
+export default App;
