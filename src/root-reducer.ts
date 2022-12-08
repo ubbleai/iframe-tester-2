6b1f5e46b9c2ff74c32f82da2998a8df5d264128
jean-baptiste <jean-baptiste@ubble.ai>
2022-12-08 10:40:08 +0100

    set Iframe2 as localforage key, fix * import

--- src/root-reducer.ts
+++ src/root-reducer.ts
@@ -1,16 +1,16 @@
-import { persistReducer } from 'redux-persist'
+import { persistReducer } from 'redux-persist';
 
-import * as localforage from 'localforage'
+import localforage from 'localforage';
 
-import { combineReducers } from '@reduxjs/toolkit'
+import { combineReducers } from '@reduxjs/toolkit';
 
-import iframeSlice from './features/iframe/store/slice'
-import logsSlice from './features/logs/store/slice'
+import iframeSlice from './features/iframe/store/slice';
+import logsSlice from './features/logs/store/slice';
 
 export const rootReducer = combineReducers({
   iframe: persistReducer(
-    { key: 'iframe', storage: localforage },
-    iframeSlice.reducer
+    { key: 'iframe2', storage: localforage },
+    iframeSlice.reducer,
   ),
-  logs: logsSlice.reducer
-})
+  logs: logsSlice.reducer,
+});
