a9f7cba096f37bf7a6bb191c6f0916c60940226d
jean-baptiste <jean-baptiste@ubble.ai>
2022-12-08 10:31:31 +0100

    Add onComplete and onExpired iframe events

--- src/features/iframe/components/Iframe.tsx
+++ src/features/iframe/components/Iframe.tsx
@@ -1,35 +1,37 @@
-import { createRef } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
+import { createRef } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
-import { createStyles } from '@mantine/core'
+import { createStyles } from '@mantine/core';
 
-import { addLog } from '../../logs/store/slice'
-import { useUbbleIframe } from '../hooks/use-ubble-iframe'
-import { selectIframeProps } from '../store/slice'
+import { addLog } from '../../logs/store/slice';
+import { useUbbleIframe } from '../hooks/use-ubble-iframe';
+import { selectIframeProps } from '../store/slice';
 
 const useStyles = createStyles({
   iframe: {
     height: '100%',
-    width: '100%'
-  }
-})
+    width: '100%',
+  },
+});
 
-export function Iframe () {
-  const { classes } = useStyles()
-  const dispatch = useDispatch()
-  const iframeRef = createRef<HTMLDivElement>()
+export function Iframe() {
+  const { classes } = useStyles();
+  const dispatch = useDispatch();
+  const iframeRef = createRef<HTMLDivElement>();
 
   const handleEvent = (event: any) => {
-    console.log(`Received event: ${event.type}: ${JSON.stringify(event)}`)
-    dispatch(addLog(JSON.stringify(event, null, 4)))
-  }
+    console.log(`Received event: ${event.type}: ${JSON.stringify(event)}`);
+    dispatch(addLog(JSON.stringify(event, null, 4)));
+  };
 
-  const ubbleProps = useSelector(selectIframeProps)
+  const ubbleProps = useSelector(selectIframeProps);
   useUbbleIframe({
     ref: iframeRef,
     ubbleProps,
-    onAbort: handleEvent
-  })
+    onAbort: handleEvent,
+    onComplete: handleEvent,
+    onExpired: handleEvent,
+  });
 
-  return <div className={classes.iframe} ref={iframeRef} />
+  return <div className={classes.iframe} ref={iframeRef} />;
 }
